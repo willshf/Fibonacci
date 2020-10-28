b38bf0da496609647ce9ebe47f9872d2f6df542f
blyat <davinquiries@gmail.com>
2020-10-28 17:40:48 +0100

    edit fibonacciRekursiv java

---
+++ FibonacciRekursiv.java
@@ -0,0 +1,11 @@
+public class FibonacciRekursiv
+{
+    public int fib(int n) {
+        if(n<=1){
+           return n;
+        }
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
