e0b969c4c7d88ac025f11264770eb371a5af5e0f
Will Shf <willi_schefner@yahoo.de>
2020-10-29 22:15:12 +0100

    chronische depressionen

--- FibonacciIterativ.java
+++ FibonacciIterativ.java
@@ -1,14 +1,47 @@
 public class FibonacciIterativ
 {
-    public int fibIterativ(int n) {
-        int i;
-        int x1 = 1; // Speicherplatz für fib(n)
-        int x2 = 1; // Speicherplatz für fib(n-1)
-        for(i=0; i<n; i++) {
-        int x3 = x2; // Merkt sich fib(n-1)
-        x2 = x1 + x2; 
-        x1 = x3;
+    public int fibonacciIterativ(int n) {
+        // n=1
+        int ErgebnisNullterDurchlauf = 1;
+        if(n==1) {
+            return ErgebnisNullterDurchlauf;
         }
-        return x1;
+        // n=2
+        int ErgebnisErsterDurchlauf = 0;
+        int VorVorherigesErgebnisErsterDurchlauf = 0;
+        
+        ErgebnisErsterDurchlauf = ErgebnisNullterDurchlauf + VorVorherigesErgebnisErsterDurchlauf;
+        if(n==2) {
+            return ErgebnisErsterDurchlauf; 
+        }
+        // n=3
+        int ErgebnisZweiterDurchlauf = 0;
+        ErgebnisZweiterDurchlauf = ErgebnisNullterDurchlauf + ErgebnisErsterDurchlauf;  
+        if(n==3){
+            return ErgebnisZweiterDurchlauf;
+        }
+        // n=4
+        int ErgebnisDritterDurchlauf = 0;
+        ErgebnisDritterDurchlauf = ErgebnisErsterDurchlauf + ErgebnisZweiterDurchlauf;
+        if(n==4) {
+            return ErgebnisDritterDurchlauf;
+        }
+        // n=5
+        int ErgebnisVierterDurchlauf = 0;
+        ErgebnisVierterDurchlauf = ErgebnisZweiterDurchlauf + ErgebnisDritterDurchlauf;
+        if(n==5) {
+            return ErgebnisVierterDurchlauf;
+        }
+        // n>=6
+        int Ergebnis = 0; 
+        int vorherigesErgebnis = 1;
+        int VorVorherigesErgebnis = 0; 
+        for(int i = 0; i<n; i++) {
+            int ZwischenErgebnis = Ergebnis;
+            Ergebnis = vorherigesErgebnis + Ergebnis;
+            VorVorherigesErgebnis = vorherigesErgebnis;
+            vorherigesErgebnis = ZwischenErgebnis;
+        }  
+        return Ergebnis;
     }
 }
